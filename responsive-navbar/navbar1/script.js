3c26332642bb8202288f63748c3b12897209c86a
aviralsharma07 <thisisavi200@gmail.com>
2023-08-29 22:20:09 +0530

    navbar2: sliding from right

--- responsive-navbar/navbar1/script.js
+++ responsive-navbar/navbar1/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
+
 hamburger.addEventListener("click", function () {
   const navbar = document.querySelector(".nav-bar");
   navbar.classList.toggle("active");
+
   const hamburger = document.querySelector(".hamburger");
   hamburger.classList.toggle("active");
 });
--- responsive-navbar/navbar2/script.js
+++ responsive-navbar/navbar2/script.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
+
 hamburger.addEventListener("click", function () {
   const navbar = document.querySelector(".nav-bar");
   navbar.classList.toggle("active");
+
   if (navbar.classList.contains("active")) {
     navbar.style.transform = "translateX(0)";
   } else {
     navbar.style.transform = "translateX(100%)";
   }
+
   const hamburger = document.querySelector(".hamburger");
   hamburger.classList.toggle("active");
 });
